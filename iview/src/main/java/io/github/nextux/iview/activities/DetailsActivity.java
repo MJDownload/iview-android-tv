f8b48190d56a25a7d587f8fe8069e3f28ab1f44f
alanmcramer <alan@cramer.id.au>
2016-12-24 22:02:25 +1030

    Started integrating base and iview components.

--- iview/src/main/java/io/github/nextux/iview/activities/DetailsActivity.java
+++ iview/src/main/java/io/github/nextux/iview/activities/DetailsActivity.java
@@ -2,7 +2,6 @@ package io.github.nextux.iview.activities;
 
 import android.os.Bundle;
 
-import io.github.nextux.base.activities.BaseActivity;
 import io.github.nextux.iview.R;
 
 public class DetailsActivity extends BaseActivity {
--- iview/src/main/java/io/github/nextux/iview/activities/MainActivity.java
+++ iview/src/main/java/io/github/nextux/iview/activities/MainActivity.java
@@ -2,7 +2,6 @@ package io.github.nextux.iview.activities;
 
 import android.os.Bundle;
 
-import io.github.nextux.base.activities.BaseActivity;
 import io.github.nextux.iview.R;
 
 public class MainActivity extends BaseActivity {
