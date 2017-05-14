fc20bb8b1ce0bf793094e05342e6c11706e5a462
Mark Starc <Mark Starc>
2017-05-13 17:02:18 -0700

    Fixed Issue #1 App crashes when usng search. App now request microphone permission. Also when TV remote search button presses, app search is called instead of TV search.

--- app/src/main/java/io/github/nextux/iview/activities/CategoryActivity.java
+++ app/src/main/java/io/github/nextux/iview/activities/CategoryActivity.java
@@ -1,5 +1,6 @@
 package io.github.nextux.iview.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import io.github.nextux.iview.R;
@@ -8,5 +9,11 @@ public class CategoryActivity extends BaseActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.category_activity);
+    }
+
+    @Override
+    public boolean onSearchRequested() {
+        startActivity(new Intent(this, SearchActivity.class));
+        return true;
     }
 }
